6d87516754c9fa1a143571e67e3deb2ee3781758
thienb2111953 <thienb2111953@student.ctu.edu.vn>
2025-12-13 18:08:05 +0700

    .

--- storage/app/python/conn.py
+++ storage/app/python/conn.py
@@ -16,8 +16,5 @@ def get_db_connection():
 PATH_PROJECT_STORAGE = Path(r"D:\Project\badminton-shop\admin-badmintonshop\storage\app")
 
 APP_URL_PATH = "http://127.0.0.1:8000/storage"
-
 API_KEY_GROQ = "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"
-API_KEY_GEMINI = "AIzaSyCwB3wewSlvRHdGMPhrD5O5MJLBrzLQzmM"
-OPENAI_API_KEY = "sk-Sqyh7tRwbF4e8Qp1-K-ElG-nbttT5Sg2Xa8eqyC7PeT3BlbkFJCtA0SX5FJMeyrqtkQW24yPbcKjCqePppjMkSeSBxoA"
 
